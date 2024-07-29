5c3846f5dcdf16bbf7dfd11daaeed0a16b951d40
sheetal149 <132652477+sheetal149@users.noreply.github.com>
2024-07-30 00:21:41 +0530

    Update click_buttons.py

--- click_buttons.py
+++ click_buttons.py
@@ -5,38 +5,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
 driver = webdriver.Chrome()
-driver.get("https://webdriveruniversity.com/Click-Buttons/index.html")
-wait = WebDriverWait(driver, 10)
 
+try:
+    
+    driver.get("https://webdriveruniversity.com/Click-Buttons/index.html")
+    
+    wait = WebDriverWait(driver, 10)
+    
+    def close_dialog():
+        try:
+            dialog = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal-dialog")))
+            close_button = dialog.find_element(By.CSS_SELECTOR, ".close")
+            close_button.click()
+            print("Dialog box closed!")
+        except Exception as e:
+            print("No dialog box to close. {e}")   
 
-def close_dialog():
-    try:
-        dialog = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal-dialog")))
-        close_button = dialog.find_element(By.CSS_SELECTOR, ".close")
-        close_button.click()
-    except Exception as e:
-        print(f"No dialog box to close: {e}")
+    # --- Regular Click ---
+    print("Waiting for Regular Click button to be present...")
+    button1 = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@id='button1']")))
+    print("Regular Click button is visible. Clicking now...")
+    button1.click()
+    print("Regular Click button clicked!")
+    time.sleep(2)  
+    close_dialog()  
 
+   # --- JavaScript Click ---
+    print("Waiting for JavaScript Click button to be present...")
+    button2 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#button2")))
+    print("JavaScript Click button is visible. Clicking now...")
+    driver.execute_script("arguments[0].click();", button2)
+    print("JavaScript Click button clicked!")
+    time.sleep(2)  
+    close_dialog()  
 
-button1 = wait.until(EC.element_to_be_clickable((By.ID, "button1")))
-button1.click()
-time.sleep(2)
-close_dialog()
+    # --- Action Move & Click ---
+    print("Waiting for Action Move & Click button to be present...")
+    button3 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#button3")))
+    print("JavaScript Click button is visible. Clicking now...")
+    driver.execute_script("arguments[0].click();", button3)
+    print("JavaScript Click button clicked!")
+    time.sleep(2)  
+    close_dialog()  
+    
+    print("Keeping the browser open for  observation...")
+    time.sleep(40)  
 
-
-button2 = wait.until(EC.element_to_be_clickable((By.ID, "button2")))
-driver.execute_script("arguments[0].click();", button2)
-time.sleep(2)
-close_dialog()
-
-
-button3 = wait.until(EC.element_to_be_clickable((By.ID, "button3")))
-ActionChains(driver).move_to_element(button3).click().perform()
-time.sleep(2)
-close_dialog()
-
-
-time.sleep(15)
-driver.quit()
+finally:
+    
+    driver.quit()
+    print("WebDriver closed.")
